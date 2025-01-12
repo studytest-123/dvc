e5d6408432553891daa2a23207affedc2f27f556
s77249 <s77249@bht-berlin.de>
2025-01-12 20:31:50 +0100

    Security Hotspot beheben (fehlende Eingrenzung der HTTP-Methode)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import StudyProgram
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET"])
 def list_study_programs():
     programs = StudyProgram.objects.all()
     data = [{"name": program.name, "description": program.short_description()} for program in programs]
