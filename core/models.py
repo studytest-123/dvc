139c96f4a09eea90c547e1717056d7878502a96d
s77249 <s77249@bht-berlin.de>
2025-01-12 21:01:34 +0100

    wiederholtes keyword argument in models.py entfernt

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class StudyProgram(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
