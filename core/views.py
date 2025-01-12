572c8ab65cba710f29be202828201bbcbf6ec190
s77249 <s77249@bht-berlin.de>
2025-01-12 20:14:36 +0100

    Security Issue 2 beheben (ungenutzter funktionsparameter(request))

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import StudyProgram
 
-def list_study_programs(request):
+def list_study_programs():
     programs = StudyProgram.objects.all()
     data = [{"name": program.name, "description": program.short_description()} for program in programs]
     return JsonResponse(data, safe=False)
